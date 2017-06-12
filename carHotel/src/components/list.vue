4dcb905e465de98be8197c656328bb000bd0cf0c
yzw <1025191631@qq.com>
2017-06-12 13:40:57 +0800

    list completer

--- carHotel/src/components/list.vue
+++ carHotel/src/components/list.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-<!--     <group title="switch the type">
-      <radio title="type" v-model="type" :options="['1', '2', '3']"></radio>
-    </group> -->
-    <panel header="酒店列表" :footer="footer" :list="list" :type="type" :rate='rate'></panel>
+    <ul>
+    	<li class="hotel-list" v-for='item in list'>
+    		<div class="hotel-img">
+    			<img :src='item.src' alt="">
+    		</div>
+    		<div class='hotel-info'>
+    			<div class="hotel-name">{{item.title}}</div>
+    			<div class="hotel-about">
+    				<span class='hotel-rate'>{{item.rate}}</span>| <span class='hotel-count'>{{item.rateCount}}条点评</span>
+    				<!-- <span ></span>	 -->
+    			</div>
+    			<div class="promotion">
+    				
+    			</div>
+    			<div class="hotel-distance">距离您{{item.distance}}</div>
+    			<div class="hotel-price">
+    				￥{{item.price}}起
+    			</div>
+    		</div>
+    	</li>
+    </ul>
   </div>
 </template>
 
@@ -22,33 +39,43 @@ export default {
         src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
         title: '标题一',
         rate:4.3,
+        rateCount:213,
         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        price:500,
+        distance:"2.1公里",
+        icon:['促','满减','可反'],
         url: '/component/cell'
       }, {
         src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-        title: '标题二',
+        title: '标题一',
         rate:4.3,
+        rateCount:213,
         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/component/radio',
-          replace: false
-        }
+        price:500,
+        distance:"2.1公里",
+        icon:['促','满减','可反'],
+        url: '/component/cell'
       },
 		{
         src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
         title: '标题一',
         rate:4.3,
+        rateCount:213,
         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        price:500,
+        distance:"2.1公里",
+        icon:['促','满减','可反'],
         url: '/component/cell'
       }, {
         src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
-        title: '标题二',
+        title: '标题一',
         rate:4.3,
+        rateCount:213,
         desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/component/radio',
-          replace: false
-        }
+        price:500,
+        distance:"2.1公里",
+        icon:['促','满减','可反'],
+        url: '/component/cell'
       }
       ],
       footer: {
@@ -59,3 +86,54 @@ export default {
   }
 }
 </script>
+
+<style>
+	.hotel-list{
+		border-bottom: 1px solid #ccc; 
+	}
+	.hotel-img,.hotel-info{
+		display: inline-block;
+	}
+	.hotel-info{
+		vertical-align:top;
+		padding-left:10px;
+		position: relative;
+		width: 64%;
+	}
+	.hotel-img{
+
+	}
+	.hotel-img>img{
+		width:100px;
+		height:100px;
+		vertical-align: bottom;
+	}
+	.hotel-name{
+		padding:4px 0;
+		font-size: 18px
+	}
+	.hotel-rate,.hotel-count{
+		display: inline-block;
+		padding-right:10px;
+		font-size:14px; 
+	}
+	.hotel-distance{
+		padding-top:8px;
+	}
+	.hotel-rate{
+		color:rgb(254,160,66);
+	}
+	.hotel-count{
+		color:#ccc;
+	}
+	.hotel-price{
+		position: absolute;
+		right:5px;
+		top:40px;
+		color:red;
+		font-size:20px;
+	}
+	.hotel-distance{
+		font-size:14px;
+	}
+</style>
